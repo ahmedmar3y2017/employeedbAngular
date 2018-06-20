ae1619e7732382534e38f6612ec3935023b930a8
ahmed mar3y <ahmedmar3y108@gmail.com>
2018-06-20 15:31:39 +0200

    هىه

--- src/app/components/employee-info/employee-info.component.ts
+++ src/app/components/employee-info/employee-info.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { AngularFirestoreCollection, AngularFirestore } from 'angularfire2/firestore';
+import { Employee } from '../Employee';
 
 @Component({
   selector: 'app-employee-info',
@@ -6,10 +10,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employee-info.component.css']
 })
 export class EmployeeInfoComponent implements OnInit {
+  private itemsCollection: AngularFirestoreCollection<Employee>;
 
-  constructor() { }
+  id: string;
 
+  employee: Employee;
+
+  constructor(private router: Router, private route: ActivatedRoute,
+    public flushMessages: FlashMessagesService,
+    private readonly db: AngularFirestore
+  ) {
+    this.itemsCollection = db.collection<Employee>('employees');
+    route.params.subscribe((params: Params) => {
+
+      this.id = params.id;
+      console.log(this.id);
+    });
+
+
+    this.getById(this.id);
+  }
   ngOnInit() {
+  }
+
+
+  getById(id) {
+
+    this.itemsCollection.doc(id).ref.get().then(function (doc) {
+      if (doc.exists) {
+        console.log("Document data:", doc.data());
+      } else {
+        console.log("No such document!");
+      }
+    }).catch(function (error) {
+      console.log("Error getting document:", error);
+    });
+
   }
 
 }
